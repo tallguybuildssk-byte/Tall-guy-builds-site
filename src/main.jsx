862038d72918cd322d82285f1167a9c277361d5e
tallguybuildssk-byte <tallguybuildssk@gmail.com>
2026-02-22 17:05:16 -0600

    fix: update routes for subdomain deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './Login'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/app" element={<Login />} />
-        <Route path="/app/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
