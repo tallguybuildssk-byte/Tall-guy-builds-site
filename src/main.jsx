90dec259b35cc5e18f41d8626dd4d49aad26cd00
tallguybuildssk-byte <tallguybuildssk@gmail.com>
2026-02-22 19:34:40 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './Login'
-const globalStyle = document.createElement('style')
-globalStyle.innerHTML = `* { margin: 0; padding: 0; box-sizing: border-box; } html, body { height: 100%; }`
-document.head.appendChild(globalStyle)
+import Dashboard from './Dashboard'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
