36c0ca04bdcbdbd6b89d07dade0319c82dc42093
tallguybuildssk-byte <tallguybuildssk@gmail.com>
2026-02-22 19:15:08 -0600

    global CSS reset

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './Login'
+const globalStyle = document.createElement('style')
+globalStyle.innerHTML = `* { margin: 0; padding: 0; box-sizing: border-box; } html, body { height: 100%; }`
+document.head.appendChild(globalStyle)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
