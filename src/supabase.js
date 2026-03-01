841f9c9bef4d449c4f693380da01acb24a71b934
tallguybuildssk-byte <tallguybuildssk@gmail.com>
2026-03-01 14:34:04 -0600

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://ptczgktyxifzbaxcsqan.supabase.co';
-const supabaseAnonKey = 'YOUR_ANON_KEY_HERE';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0Y3pna3R5eGlmemJheGNzcWFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2MjY3OTUsImV4cCI6MjA4NzIwMjc5NX0.5JBo2FiwWub-qhEWBT3al6PaEJVXrxGscNOUN1ubooQ';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
