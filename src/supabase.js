ea5384486dfa932432402895cc9736007ce475ef
tallguybuildssk-byte <tallguybuildssk@gmail.com>
2026-03-02 19:31:35 -0600

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -3,4 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://ptczgktyxifzbaxcsqan.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0Y3pna3R5eGlmemJheGNzcWFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2MjY3OTUsImV4cCI6MjA4NzIwMjc5NX0.5JBo2FiwWub-qhEWBT3al6PaEJVXrxGscNOUN1ubooQ';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
